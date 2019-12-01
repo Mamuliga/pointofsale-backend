2c14d3b0df80f4ec6c6105c43e3bbee5d715fdf9
Mahmood <mahmood@Mahmoods-MacBook-Pro.local>
2019-12-01 07:20:52 +0530

    mock router api

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import authRouter from "./auth";
 import employeeRoute from "./employee";
-
+import mockRouter from "./mock"
 const apiRoute = Router();
 
 apiRoute.use("/auth", authRouter);
 apiRoute.use("/employees", employeeRoute);
-
+apiRoute.use("/mock",mockRouter);
 export default apiRoute;
