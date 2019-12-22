fa722daca95609e7fd09b1237099e5e32a7bc16d
Rizan Mohomed <rmohomed@mitrai.com>
2019-12-22 11:40:38 +0530

    removed build folder

--- build/config/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Object.assign({ PORT: 3300, APP_NAME: "EIT POS", APP_VERSION: "1.0.0-dev", DB_HOST: "localhost", DB_NAME: "eit-pos", DB_USER: "root", DB_PASSWORD: "", DB_PORT: 3306 }, process.env);
-//# sourceMappingURL=index.js.map
