c6fc2949b159bd21d66bab3b5383adf7d3298ef9
Marjan Mukram <marjanmukram8@gmail.com>
2020-02-16 23:43:51 +0530

    Customer id se

--- src/controllers/apiShapes/Customer.ts
+++ src/controllers/apiShapes/Customer.ts
@@ -1,5 +1,6 @@
 export function Customer(customer: any) {
   return {
+    id:customer && customer.id,
     firstName: customer && customer.firstName,
     lastName: customer && customer.lastName,
     phoneNo: customer && customer.phoneNo,
