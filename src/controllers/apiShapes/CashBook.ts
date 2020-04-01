19001cb2854c8c9b33fd88755ca44f76b3cc7c6d
Marjan Mukram <marjanmukram8@gmail.com>
2020-04-01 19:22:28 +0530

    cash book date bugs fixed

--- src/controllers/apiShapes/CashBook.ts
+++ src/controllers/apiShapes/CashBook.ts
@@ -1,7 +1,6 @@
 export function CashbookShape(cashBook: any) {
     return {
       id:cashBook && cashBook.id,
-      date: cashBook && cashBook.date,
       refNo: cashBook && cashBook.refNo,
       description: cashBook && cashBook.description,
       type: cashBook && cashBook.type,
--- src/interfaces/ICashBook.ts
+++ src/interfaces/ICashBook.ts
@@ -1,5 +1,4 @@
 export default interface ICashBook {
-    date: string;
     refNo: string;
     description: string;
     type: string;
