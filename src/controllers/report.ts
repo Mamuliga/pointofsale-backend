0599a50e620bded5be9c3a0bb9f5ae54258484be
imran_zarook <imranZarook.emeraldit@gmail.com>
2020-06-05 20:03:18 -0700

    Receives report by payment type API

--- src/controllers/report.ts
+++ src/controllers/report.ts
@@ -6,6 +6,7 @@ import {
   getSalesReportByPaymentType,
   getLowInventoryReport,
   getReceivesByDateRange,
+  getReceivesReportByPaymentType
 } from "../models/Report";
 import { SalesShape } from "./apiShapes/Report";
 
@@ -110,6 +111,23 @@ reportRoute.get("/receives-by-date-range", async (req, res) => {
       return;
     }
     res.status(200).json(receivesByDate);
+  } catch (ex) {
+    console.log(ex);
+    res.status(res.statusCode || 400).json({
+      error: ex.message,
+    });
+  }
+});
+
+reportRoute.get("/receives-report-by-payment-type", async (req, res) => {
+  try {
+    const receivesByPaymentType = await getReceivesReportByPaymentType(req.query);
+
+    if (!receivesByPaymentType) {
+      res.status(204).json({});
+      return;
+    }
+    res.status(200).json(receivesByPaymentType);
   } catch (ex) {
     console.log(ex);
     res.status(res.statusCode || 400).json({
