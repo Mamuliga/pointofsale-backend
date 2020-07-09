376a75fc648012f26e4f41651851d77dbfb06dcc
imran_zarook <imranZarook.emeraldit@gmail.com>
2020-07-09 22:33:33 +0530

    removed comment

--- src/interfaces/IItemStat.ts
+++ src/interfaces/IItemStat.ts
@@ -2,7 +2,6 @@ export default interface IItemStats {
   itemId: number;
   supplierId: number;
   costPrice: number;
-  // salesPrice: string;
   manuDate: Date;
   expDate: Date;
   quantity: number;
